15963486a89c0b4d528d9bdd51b3fbbc067e8d2c
Anthony <tony464us2011@yahoo.com>
2023-01-24 21:56:08 -0500

    added semantic html

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <header>
+      <div id="logo"><img src="/logo.png" /></div>
+        <Nav />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+      <nav>
+        <ul>
+          <li>
+            <a href='!#'>Link</a>
+          </li>
+          <li>
+            <a href='!#'>Link</a>
+          </li>
+          <li>
+            <a href='!#'>Link</a>
+          </li>
+          <li>
+            <a href='!#'>Link</a>
+          </li>
+        </ul>
+      </nav>
+  )
+}
+
+export default Nav
